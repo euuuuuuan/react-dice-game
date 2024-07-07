d6949f0213b65537ecff806111c5fe0203d15a16
euan.may24@gmail.com <euan.may24@gmail.com>
2024-07-07 19:38:03 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Dice from './Dice';
+import Button from "./Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <Button text="던지기" />
+                <Button text="처음부터" />
+            </div>
+            <Dice color="red" num={2}/>
+        </div>
+    );
 }
 
 export default App;
