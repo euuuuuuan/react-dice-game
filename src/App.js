d7a9f2bc1f67c463e79fc45d0e7793aca8513f29
euan.may24@gmail.com <euan.may24@gmail.com>
2024-07-08 21:27:23 +0900

    [Feat]주사위 기록기능 추가

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ function random(n) {
 
 function App() {
     const [num, setNum] = useState(1);
+    const [sum, setSum] = useState(0);
+    const [gameHistory, setGameHistory] = useState([]);
     /* 스테이터와 세터를 배열 형태로 리턴하기 때문에 Destructuring 문법을 작성한다.
     * state를 변경할 때는 직접 값을 변경하지 않고 반드시 setter함수를 통해 변경한다. */
 
     const handleRollClick = () => {
         const nextNum = random(6);
+        /* 배열은 기본형이 아니라 참조형이다. gameHistory 변수는 기록들을 가진 배열 자체를 값으로 갖지 않고
+        * 그 배열을 가리키는 주소값을 갖고 있다.*/
         setNum(nextNum);
+        setSum(sum + nextNum);   // setNum과 setSum 부분을 주석처리 하면 아래의 기록용 코드도 작동하지 않는다.
+        // gameHistory.push(nextNum);  // gameHistory가 배열이니까 push 메소드로 nextNum 추가
+        // setGameHistory(gameHistory);
+        setGameHistory([...gameHistory, nextNum]); // 위의 문제점을 해결하기 위해 Spread 문법을 사용한다.
+        /* 배열이나 객체 같은 참조형 State를 사용할 때는 메소드나 할당 연산자로 값을 바꾸지 않고 반드시 새로운 값을 만들어서 변경한다. */
     };
     const handleClearClick = () => {
         setNum(1);
+        setSum(0);
+        setGameHistory([]);  // 빈 배열 추가
     }
 
     return (
@@ -25,7 +36,14 @@ function App() {
                 <Button onClick={handleRollClick}>던지기</Button>
                 <Button onClick={handleClearClick}>처음부터</Button>
             </div>
-            <Dice color="red" num={num}/>
+            <div>
+                <h2>나</h2>
+                <Dice color="blue" num={num}/>
+                <h2>총점</h2>
+                <p>{sum}</p>
+                <h2>기록</h2>
+                <p>{gameHistory.join(', ')}</p>
+            </div>
         </div>
     );
 }
